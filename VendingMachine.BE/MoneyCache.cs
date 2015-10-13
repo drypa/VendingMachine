a12d73638a824d3711a85d1f085118b8788c258c
DRypa <dr.drypa.k@mail.ru>
2015-10-13 23:06:55 +0300

    added entities for money

---
+++ VendingMachine.BE/MoneyCache.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VendingMachine.BE
+{
+    public class MoneyCache
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public double Nominal { get; set; }
+        public int Count { get; set; }
+    }
+}
--- VendingMachine.BL/VendingContext.cs
+++ VendingMachine.BL/VendingContext.cs
@@ -6,5 +6,7 @@ namespace VendingMachine.BL
     public class VendingContext: DbContext
     {
         public DbSet<ItemToSale> ItemsToSale { get; set; }
+        public DbSet<MoneyCache> MoneyСache { get; set; }
+        public DbSet<Bank> Bank { get; set; }
     }
 }
