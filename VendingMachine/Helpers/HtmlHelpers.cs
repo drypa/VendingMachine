a8dd8176f99c42fb1f7a007ee0cd2119c723b17f
unknown <KrasavinAA@SPBDEVELOPER42.corp.loc>
2015-10-15 11:56:09 +0300

    helper added to include action mapped js

---
+++ VendingMachine/Helpers/HtmlHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VendingMachine.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static MvcHtmlString IncludeJs(this HtmlHelper helper)
+        {
+            string result = string.Empty;
+            string controllerName = helper.ViewContext.RouteData.Values["controller"].ToString();
+            string actionName = helper.ViewContext.RouteData.Values["action"].ToString();
+            const string pattern = "<script type='text/javascript' src='{0}'></script>";
+            string commonControllerJs = GetScriptPath(controllerName, controllerName, "js");
+            if (File.Exists(HttpContext.Current.Server.MapPath(commonControllerJs)))
+            {
+                result += string.Format(pattern, commonControllerJs);
+            }
+
+            string actionJs = GetScriptPath(controllerName, actionName, "js");
+            if (File.Exists(HttpContext.Current.Server.MapPath(actionJs)))
+            {
+                result += string.Format(pattern, actionJs);
+            }
+
+            return new MvcHtmlString(result);
+        }
+
+
+        private static string GetScriptPath(string controller, string file, string extention)
+        {
+            return string.Format("~/Scripts/Pages/{0}/{1}.{2}", controller, file, extention);
+        }
+    }
+}
