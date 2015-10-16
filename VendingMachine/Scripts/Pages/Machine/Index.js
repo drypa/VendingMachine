4ceeb18231a53091a77fec90302630684a799d06
DRypa <dr.drypa.k@mail.ru>
2015-10-16 22:37:52 +0300

    small js refactoring: right action url for ajax requests

--- VendingMachine/Scripts/Pages/Machine/Index.js
+++ VendingMachine/Scripts/Pages/Machine/Index.js
@@ -1,44 +1,46 @@
-﻿$(document).ready(function () {
-    $('[data-id]').click(function () {
-        var productId = $(this).data('id');
-        $.ajax({
-            method: 'POST',
-            url: 'Machine/Buy',
-            data: { productId: productId },
-            success: function (data, textStatus, jqXHR) {
-                if (data.status == 'ok') {
-                    window.location = window.location;
-                } else {
-                    alert(data.message);
+﻿(function ($, document,VM) {
+    $(document).ready(function () {
+        $('[data-id]').click(function () {
+            var productId = $(this).data('id');
+            $.ajax({
+                method: 'POST',
+                url: VM.BuyUrl,
+                data: { productId: productId },
+                success: function (data, textStatus, jqXHR) {
+                    if (data.status == 'ok') {
+                        window.location = window.location;
+                    } else {
+                        alert(data.message);
 
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert('error');
                 }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert('error');
-            }
+            });
+            return false;
         });
-        return false;
-    });
 
-    $('[data-nominal]').click(function () {
-        var nominal = $(this).data('nominal');
-        $.ajax({
-            method: 'POST',
-            url: 'Machine/Insert',
-            data: { nominal: nominal },
-            success: function (data, textStatus, jqXHR) {
-                if (data.status == 'ok') {
-                    window.location = window.location;
-                } else {
-                    alert(data.message);
+        $('[data-nominal]').click(function () {
+            var nominal = $(this).data('nominal');
+            $.ajax({
+                method: 'POST',
+                url: VM.InsertCoin,
+                data: { nominal: nominal },
+                success: function (data, textStatus, jqXHR) {
+                    if (data.status == 'ok') {
+                        window.location = window.location;
+                    } else {
+                        alert(data.message);
 
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert('textStatus');
                 }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert('textStatus');
-            }
+            });
+            return false;
         });
-        return false;
-    });
 
-});
+    });
+})(jQuery, document, window.VM);
